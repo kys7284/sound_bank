57e5bda79f3efb6a47ce28cada2c68a21a5e1f8b
kys7284 <tbvjskql@naver.com>
2025-03-22 14:40:41 +0900

    환경설정

---
+++ src/main/java/com/boot/sound/SoundBankApplication.java
@@ -0,0 +1,13 @@
+package com.boot.sound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/sound/SoundBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.sound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
