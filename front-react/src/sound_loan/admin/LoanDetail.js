af588ab553bd5921057dd6d23f3369903337a588
daeyeol1003 <dayoul222@nate.com>
2025-03-24 15:42:09 +0900

    [Header.js, App.js] 대출/이체/조회 Link & Routes 추가
    관련 폴더 & 파일 생성 / 링크연결 완료

--- front-react/src/sound_loan/admin/LoanDetail.js
+++ front-react/src/sound_loan/admin/LoanDetail.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import "../../Css/loan/Loan.css";
 
 const LoanDetail = (props) => {
   const { loan_id } = useParams();
+  const navigate = useNavigate();
 
   const [loan, setLoan] = useState([
     {
@@ -23,14 +25,36 @@ const LoanDetail = (props) => {
       });
   }, [loan_id]);
 
+  const updateForm = () => {
+    navigate("/loanUpdate/" + loan_id);
+  };
+
+  const deleteForm = () => {
+    fetch("http://localhost:8081/api/loanDelete/" + loan_id, {
+      method: "DELETE",
+    })
+      .then((res) => res.text())
+      .then((res) => {
+        if (res === "ok") {
+          alert("삭제 되었습니다.");
+          navigate("/loanList");
+        } else {
+          alert("삭제 실패");
+          navigate("/loanList");
+        }
+      });
+  };
+
   return (
-    <div>
+    <div className="loan">
       <p>대출상품 번호 :{loan.loan_id}</p>
       <p>대출상품 이름 :{loan.loan_name} </p>
       <p>대출상품 금리 :{loan.interest_rate}</p>
       <p>대출상품 기간 :{loan.loan_term}</p>
       <p>대출상품 한도 :{loan.loan_amount}</p>
       <p>대출상품 정보 :{loan.loan_info}</p>
+      <button onClick={updateForm}>수정하기</button>
+      <button onClick={deleteForm}>삭제하기</button>
     </div>
   );
 };
