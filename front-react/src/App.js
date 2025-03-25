3fc41d65fb45b574e5b92f1d2a9a6753b88f5a51
daeyeol1003 <dayoul222@nate.com>
2025-03-25 22:00:37 +0900

    [LOAN]  벤치마킹 화면 이미지 삽입 작업 진행중

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -70,6 +70,9 @@ import AdminExAccountStatement from "./exchange/admin/AdminExAccountStatement";
 import AdminExLimit from "./exchange/admin/AdminExLimit";
 import AdminExSetCharge from "./exchange/admin/AdminExSetCharge";
 import LoanInsertForm from "./sound_loan/admin/LoanInsertForm";
+import LoanCalculator from "./sound_loan/customer/LoanCalculator";
+import LoanChart from "./sound_loan/customer/LoanChart";
+import LoanCreditScore from "./sound_loan/customer/LoanCreditScore";
 function App() {
   return (
     <div className="App">
@@ -124,6 +127,9 @@ function App() {
           <Route path="/loanStatus" element={<LoanStatus />} />
           <Route path="/loanManage" element={<LoanManage />} />
           <Route path="/loanService" element={<LoanService />} />
+          <Route path="/loanCalculator" element={<LoanCalculator />} />
+          <Route path="/loanChart" element={<LoanChart />} />
+          <Route path="/loanCreditScore" element={<LoanCreditScore />} />
           <Route path="/loanList" element={<LoanList />} />
           <Route path="/loanInsertForm" element={<LoanInsertForm />} />
           <Route path="/loanDetail/:loan_id" element={<LoanDetail />} />
--- front-react/src/sound_loan/customer/LoanStatus.js
+++ front-react/src/sound_loan/customer/LoanStatus.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const LoanStatus = () => {
-  return <div></div>;
+  return (
+    <div style={{ minHeight: 600 }}>
+      <img
+        src="/Images/LOAN/LoanStatus.png"
+        alt="벤치마킹화면"
+        style={{
+          display: "block",
+          margin: "0 auto",
+          width: "600px",
+          height: "auto",
+          border: "1px solid black",
+        }}
+      />
+    </div>
+  );
 };
 
 export default LoanStatus;
