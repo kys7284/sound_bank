17114c96abb000c3bd373084cb54cbb2f91b9eec
Serena Kim <ehdud44777@gmail.com>
2025-03-25 15:12:37 +0900

    펀드 목록 띄우기완료
    펀드 추천AI 진행중

---
+++ bin/front-react/src/customer_center/Customerservice.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Customerservice = () => {
+  return <div></div>;
+};
+
+export default Customerservice;
---
+++ bin/front-react/src/sound_loan/admin/LoanList.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../../Css/loan/Loan.css";
+
+const LoanList = (props) => {
+  const [loanList, setLoanList] = useState([
+    {
+      loan_id: 0,
+      loan_name: "",
+      loan_min_amount: 0, // 최소 대출 금액
+      loan_max_amount: 0, // 최대 대출 금액
+      interest_rate: 0.0,
+      loan_term: 0,
+      loan_info: "",
+      loan_type: "",
+    },
+  ]);
+
+  useEffect(() => {
+    fetch("http://localhost:8081/api/loanList", {
+      method: "GET",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((res) => {
+        setLoanList(res);
+      })
+      .catch((error) => {
+        console.error("데이터 가져오기 오류:", error);
+        // 사용자에게 오류 메시지를 표시하거나 다른 오류 처리 로직을 추가할 수 있습니다.
+      });
+  }, []);
+
+  return (
+    <div className="loanList">
+      <table className="listTable">
+        <thead>
+          <tr>
+            <h2>대출 상품 목록</h2>
+          </tr>
+          <tr>
+            <th>대출 아이디</th>
+            <th>대출 상품명</th>
+            <th>대출 유형</th>
+            <th>최소 대출 금액</th>
+            <th>최대 대출 금액</th>
+            <th>이자율</th>
+            <th>대출기간</th>
+            <th>대출정보</th>
+            <th>관리</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loanList.map((loan) => (
+            <tr key={loan.loan_id}>
+              <td>{loan.loan_id}</td>
+              <td>{loan.loan_name}</td>
+              <td>{loan.loan_type}</td>
+              <td>
+                {loan.loan_min_amount >= 10000
+                  ? `${(loan.loan_min_amount / 10000).toLocaleString()} 억원` // 10,000만원 이상일 경우 억원 단위로 표기
+                  : `${loan.loan_min_amount.toLocaleString()} 만원`}
+              </td>
+              <td>
+                {loan.loan_max_amount >= 10000
+                  ? `${(loan.loan_max_amount / 10000).toLocaleString()} 억원` // 10,000만원 이상일 경우 억원 단위로 표기
+                  : `${loan.loan_max_amount.toLocaleString()} 만원`}
+              </td>
+              <td>{loan.interest_rate}</td>
+              <td>{loan.loan_term}</td>
+              <td>{loan.loan_info}</td>
+              <td>
+                <Link to={"/loanDetail/" + loan.loan_id}>상세보기</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default LoanList;
