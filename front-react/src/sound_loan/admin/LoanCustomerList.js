87c8555a29c8b7964f8fa7eaecde6304f35671c7
daeyeol1003 <dayoul222@nate.com>
2025-03-26 14:45:52 +0900

    [loan 벤치마킹 화면캡쳐 등록, 관리자 CRUD기능 작업완]

--- front-react/src/sound_loan/admin/LoanCustomerList.js
+++ front-react/src/sound_loan/admin/LoanCustomerList.js
@@ -1,7 +1,64 @@
 import React from "react";
+import "../../Css/loan/Loan.css";
 
 const LoanCustomerList = () => {
-  return <div style={{ minHeight: 600 }}></div>;
+  return (
+    <div style={{ minHeight: 600 }} className="loanInsert">
+      <table className="insertTable">
+        <thead>
+          <tr>
+            <h2>대출 가입 고객 목록</h2>
+          </tr>
+          <tr>
+            <th>고객 아이디</th>
+            <th>대출 상품명</th>
+            <th>대출 금액</th>
+            <th>잔여 대출금</th>
+            <th>상환 계좌번호</th>
+            <th>대출 상환방식</th>
+            <th>대출금 상환일</th>
+            <th>대출 진행상황</th>
+            <th>관리</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>minjong123</td>
+            <td>직장인 신용대출</td>
+            <td>2억원</td>
+            <td>2억원</td>
+            <td>111-1234-9999-250</td>
+            <td>원리금 균등상환</td>
+            <td>매달 27일</td>
+            <td>대출 승인</td>
+            <td>승인처리</td>
+          </tr>
+          <tr>
+            <td>youngsuck123</td>
+            <td>직장인 신용대출</td>
+            <td>5억원</td>
+            <td>2억 5천만원</td>
+            <td>111-2244-7890-250</td>
+            <td>원리금 균등상환</td>
+            <td>매달 15일</td>
+            <td>진행중</td>
+            <td>승인처리</td>
+          </tr>
+          <tr>
+            <td>daeyeol123</td>
+            <td>직장인 신용대출</td>
+            <td>3천만원</td>
+            <td>0원</td>
+            <td>111-4233-2222-250</td>
+            <td>원리금 균등상환</td>
+            <td>매달 1일</td>
+            <td>만기</td>
+            <td>승인처리</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default LoanCustomerList;
--- front-react/src/sound_loan/customer/LoanManage.js
+++ front-react/src/sound_loan/customer/LoanManage.js
@@ -4,11 +4,11 @@ const LoanManage = () => {
   return (
     <div style={{ minHeight: 600 }}>
       <img
-        src="/Images/LOAN/LoanManage.png"
+        src="/Images/loan/LoanManage.png"
         alt="벤치마킹화면"
         style={{
           display: "block",
-          margin: "0 auto",
+          margin: "100px auto",
           width: "600px",
           height: "auto",
           border: "1px solid black",
