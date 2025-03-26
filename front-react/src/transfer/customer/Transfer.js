6f1ee9c1ab879ec08a118a5d46e93546b2a6d7e8
hikim123 <jinjiang1128@naver.com>
2025-03-26 10:33:54 +0900

    조회/이체 캡쳐완

--- front-react/src/transfer/customer/Transfer.js
+++ front-react/src/transfer/customer/Transfer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Transfer = () => {
-  return <div></div>;
+  return (
+    <div style={{ backgroundColor: "#ffffff", minHeight: "100vh", padding: "2rem" }}>
+      <img
+        src="./Images/inquire_transfer/TransInstant.png" 
+        alt=""
+        style={{ width: "100%", border: "1px solid #ddd", borderRadius: "8px" }}
+      />
+    </div>
+  );
 };
+
 
 export default Transfer;
