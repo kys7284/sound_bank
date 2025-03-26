43a3dacc0f11d46415a3d665c86fae1c862442ce
kys7284 <tbvjskql@naver.com>
2025-03-26 14:35:19 +0900

    펀드 사진추가

--- front-react/src/fund/customer/FundList.js
+++ front-react/src/fund/customer/FundList.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const FundList = () => {
-  return <div></div>;
+  return (
+    <div style={{ minHeight: 600 }}>
+      <img
+        src="/Images/FUND/FundList.png"
+        alt="펀드리스트"
+        style={{
+          display: "block",
+          margin: "0 auto",
+          width: "600px",
+          height: "auto",
+          border: "1px solid black",
+        }}
+      />
+    </div>
+  );
 };
 
 export default FundList;
