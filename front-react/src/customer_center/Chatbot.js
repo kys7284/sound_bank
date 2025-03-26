a77d2569771af13b87c6497de778f1d5ed09bda3
DESKTOP-AGA8AHS\Jang <foolg106@gmail.com>
2025-03-26 13:11:22 +0900

    고객센터이미지추가

--- front-react/src/customer_center/Chatbot.js
+++ front-react/src/customer_center/Chatbot.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
 
-const Chatbot = () => {
-  return <div></div>;
-};
+function Chatbot() {
+  return (
+    <div style={{ minHeight: 600 }}>
+  <img
+    src="/Images/customer_center/Chatbot.png"
+    alt="챗봇"
+    style={{
+      display: "block",
+      margin: "0 auto",
+      width: "100%",
+      height: "auto",
+      border: "1px solid black",
+    }}
+  />
+
+</div>
+  );
+}
 
 export default Chatbot;
