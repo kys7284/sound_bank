83955734975e4eb42eabf9c85e985208d0c3a04d
kim seog gyun <tjrrbs95@gmail.com>
2025-03-31 14:53:57 +0900

    환전신청작업중

--- front-react/src/exchange/customer/ExCreateAccount.js
+++ front-react/src/exchange/customer/ExCreateAccount.js
@@ -8,45 +8,15 @@ const ExCreateAccount = () => {
         <table>
           <tbody>
             <tr>
-              <th>
-                <label htmlFor="exchange_account_number">외환 계좌번호 :</label>
-              </th>
-              <td>
-                <input
-                  type="text"
-                  id="exchange_account_number"
-                  name="exchange_account_number"
-                />
-              </td>
+              추후에 신분증 실명인증으로 변경              
             </tr>
-            <tr>
-              <th>
-                <label htmlFor="exchange_account_name">계좌명 :</label>
-              </th>
-              <td>
-                <input
-                  type="text"
-                  id="exchange_account_name"
-                  name="exchange_account_name"
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>
-                <label htmlFor="base_currency">기준통화 :</label>
-              </th>
-              <td>
-                <input
-                  type="text"
-                  id="base_currency"
-                  name="base_currency"
-                />
-              </td>
-            </tr>
+            <tr>              
+              통장을 만들어보자.
+            </tr>            
             </tbody>
           </table>
         </form>      
-          <button type="submit">계좌 신청</button> 
+          <button type="submit">다음</button> 
                    
   </div>
   )
--- src/main/java/com/boot/sound/exchange/ExchangeServiceImpl.java
+++ src/main/java/com/boot/sound/exchange/ExchangeServiceImpl.java
@@ -1,33 +1,40 @@
 package com.boot.sound.exchange;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.client.LaxRedirectStrategy;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class ExchangeServiceImpl {
 
     private final String apiKey = "TzsQ31CAai0yWB3qXIhrtFyxqpxNO7H6";
-
     // 리디렉션 처리 가능한 HttpClient 설정
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public ExchangeServiceImpl() {
+   
+    private final ExchangeDAO dao;
+    
+    public ExchangeServiceImpl(ExchangeDAO dao) {
+    	
+    	this.dao = dao;
+    	
     	HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
     	factory.setHttpClient(HttpClients.custom().disableRedirectHandling().build());
-        this.restTemplate = new RestTemplate(factory);
+        this.restTemplate = new RestTemplate(factory);		
     }
 
+    //환율 리스트
     public List<Map<String, Object>> getExchangeRates(String date) {
         try {
             if (date == null || date.isEmpty()) {
@@ -50,5 +57,11 @@ public class ExchangeServiceImpl {
             e.printStackTrace();
             throw new RuntimeException("환율 정보 조회 중 오류 발생", e);
         }
+    }
+    
+    @Transactional
+    public ExchangeRequestDTO requestExchange(ExchangeRequestDTO dto) {
+        dao.requestExchange(dto); // dto에 exchange_request_id 채워짐
+        return dao.selectExchangeRequestById(dto.getExchange_request_id());
     }
 }
