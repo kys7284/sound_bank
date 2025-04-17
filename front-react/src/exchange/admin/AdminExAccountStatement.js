5c609028fabfcc903721742f62a0605c229cd4f1
banchanking <tjrrbs95@gmail.com>
2025-04-18 01:24:21 +0900

    ELK
    awsDB연결 완료, exchange_transaction dashboard 생성완료

--- front-react/src/exchange/admin/AdminExAccountStatement.js
+++ front-react/src/exchange/admin/AdminExAccountStatement.js
@@ -7,8 +7,8 @@ const AdminExAccountStatement = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
       <iframe 
-        src="http://localhost:5601/app/dashboards#/view/de53cf50-1b55-11f0-9e74-5f51300943a0?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow%2Fd%2Cto%3Anow%2Fd))&show-time-filter=true" 
-        height="600" 
+        src="http://localhost:5601/app/dashboards#/view/6913f560-1b98-11f0-9ab3-9f91ea40a18b?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!f%2Cvalue%3A10000)%2Ctime%3A(from%3Anow-24h%2Cto%3Anow))&show-time-filter=true" 
+        height="800" 
         width="1200"
         frameBorder="0"
         title="Kibana Dashboard">
