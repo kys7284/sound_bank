6ad39cdf0e25bbc1d49c485b0010f008442f218c
kys7284 <tbvjskql@naver.com>
2025-04-18 18:15:14 +0900

    예금 중간저장

--- src/main/java/com/boot/sound/deposit/DepositController.java
+++ src/main/java/com/boot/sound/deposit/DepositController.java
@@ -67,23 +67,23 @@ public class DepositController {
 	       }
 	   }
 	   
-	    // 자동이체 등록
-	    @PostMapping("/autoTransfer")
-	    public String add(@RequestBody DepositDTO dto) {
-	        // 비밀번호 확인
-	        boolean ok = service.checkPassword(dto.getOut_account_number(), dto.getPassword());
-
-	        if (!ok) {
-	            return "비밀번호 오류";
-	        }
-
-	        // 누락될 수 있는 기본값 보완
-	        dto.setTransfer_type("자동");   // 이체 유형 고정
-	        dto.setActive_yn("Y");          // 기본 사용 상태
-
-	        service.saveTransAuto(dto);
-	        
-	        return "자동이체 등록완료";
-	    }
+//	    // 자동이체 등록
+//	    @PostMapping("/autoTransfer")
+//	    public String add(@RequestBody DepositDTO dto) {
+//	        // 비밀번호 확인
+//	        boolean ok = service.checkPassword(dto.getOut_account_number(), dto.getPassword());
+//
+//	        if (!ok) {
+//	            return "비밀번호 오류";
+//	        }
+//
+//	        // 누락될 수 있는 기본값 보완
+//	        dto.setTransfer_type("자동");   // 이체 유형 고정
+//	        dto.setActive_yn("Y");          // 기본 사용 상태
+//
+//	        service.saveTransAuto(dto);
+//	        
+//	        return "자동이체 등록완료";
+//	    }
 
 }
